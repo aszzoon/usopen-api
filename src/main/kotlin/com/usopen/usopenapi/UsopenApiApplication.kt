f79d841b23aecf72ace56a97470e74e766c8fbef
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2025-03-27 19:05:11 +0900

    init

---
+++ src/main/kotlin/com/usopen/usopenapi/UsopenApiApplication.kt
@@ -0,0 +1,11 @@
+package com.usopen.usopenapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsopenApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<UsopenApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/usopen/usopenapi/UsopenApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.usopen.usopenapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UsopenApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
